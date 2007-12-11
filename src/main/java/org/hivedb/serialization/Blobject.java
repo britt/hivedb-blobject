a421b643055f1af416be6c8bcd0c6af15dab1aad
Britt Crawford <britt@illtemper.org>
2007-12-11 22:07:19 +0000

    Added resource declaration for testing to the pom

--- src/main/java/org/hivedb/serialization/Blobject.java
+++ src/main/java/org/hivedb/serialization/Blobject.java
@@ -2,9 +2,12 @@ package org.hivedb.serialization;
 
 import java.sql.Blob;
 
+import org.hivedb.annotations.Ignore;
+
 public class Blobject implements Blobbable {
 	protected Blob blob;
 
+	@Ignore
 	public Blob getBlob() {
 		return blob;
 	}
